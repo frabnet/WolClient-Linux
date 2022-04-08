fe4c44c5248eaa0729a06b7d3983c3bc7e0baca0
Frab <frabnet@users.noreply.github.com>
2022-04-08 15:49:07 +0200

    First upload

---
+++ wolclient.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Sending request..."
+
+#Read configuration
+. "./wolclient.config"
+COOKIEFILE="wolclient.cookie"
+
+#Homepage
+RESPONSE=$(curl -sS --insecure --location --cookie-jar "$COOKIEFILE" "$PFSENSEURL")
+CSRF=$(echo $RESPONSE | grep -Po "(?<=csrfMagicToken = \")(.*?)(?=\")")
+
+#Login
+RESPONSE=$(curl -sS --insecure --location --cookie "$COOKIEFILE" --cookie-jar "$COOKIEFILE" "$PFSENSEURL" --data "__csrf_magic=$CSRF&login=Login&usernamefld=$PFSENSEUSER&passwordfld=$PFSENSEPASSWORD")
+CSRF=$(echo $RESPONSE | grep -Po "(?<=csrfMagicToken = \")(.*?)(?=\")")
+
+#WakeOnLan
+RESPONSE=$(curl -sS --insecure --location --cookie "$COOKIEFILE" --cookie-jar "$COOKIEFILE" "$PFSENSEURL/services_wol.php" --data "__csrf_magic=$CSRF&if=lan&mac=$MAC&Submit=Send")
+CSRF=$(echo $RESPONSE | grep -Po "(?<=csrfMagicToken = \")(.*?)(?=\")")
+STATUS=$(echo $RESPONSE | grep -Po "(?<=\<\/button\>\<div class=\"pull-left\"\>)(.*?)(?=\<)")
+echo "Result: $STATUS"
+
+#Logout
+RESPONSE=$(curl -sS --insecure --location --cookie "$COOKIEFILE" --cookie-jar "$COOKIEFILE" "$PFSENSEURL/index.php?logout" --data "__csrf_magic=$CSRF&logout=\"\"")
+
+rm "$COOKIEFILE"
